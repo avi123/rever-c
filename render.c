3d5af499bbe8d0b710e4ec47e068e18c712fd8b2
Avi Berkowitz <avi.berkowitz@gmail.com>
2012-02-27 23:41:47 -0500

    added render function

---
+++ render.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define WHITE ((((long int)1 << 9) + 1) << 27)
+#define BLACK ((((long int)1 << 7) + 1) << 28)
+
+void horizontalline(int charlength);
+void render(long int black, long int white);
+
+int main() {
+    render(BLACK,WHITE);
+}
+
+void render(long int black, long int white) {
+    int row,column;
+
+    horizontalline(33);
+    for(row = 1; row <= 8; row++) {
+        printf("|"); 
+        for(column = 1; column <= 8; column++, black>>=1, white>>=1) {
+            if(black & 1)
+                printf(" B |");
+            else if(white & 1)
+                printf(" W |");
+            else
+                printf("   |");
+        }
+        printf("\n");
+        horizontalline(33);
+    }
+}
+
+void horizontalline(int charlength) {
+    int i;
+
+    for(i = 1; i <= charlength; i++)
+        printf("-");
+    printf("\n");
+}
