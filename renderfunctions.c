ed905bff9b34b762ff3eb43ad4c0aadd63efa22d
Avi Berkowitz <avi.berkowitz@gmail.com>
2012-02-29 12:33:36 -0500

    removed defines from renderfunctions.c

--- renderfunctions.c
+++ renderfunctions.c
@@ -2,14 +2,6 @@
 #include <string.h>
 #include "renderfunctions.h"
 
-#define EMPTY ((long unsigned int)1 << 63) + 2
-#define EXIT ((long unsigned int)1 << 63) + 1
-#define VALID ((long unsigned int)1 << 63) + 4
-#define INIT_WHITE ((((long unsigned int)1 << 9) + 1) << 27)
-#define INIT_BLACK ((((long unsigned int)1 << 7) + 1) << 28)
-#define MAXMOVES 64
-#define ROWLENGTH 33
-
 void decompose(long unsigned int moves,long unsigned int decomposedmoves[]) {
     int i=0,j=0;
 
