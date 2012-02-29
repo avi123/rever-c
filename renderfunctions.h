403b42096c969ab2539f8195382d5c8e6007749b
Avi Berkowitz <avi.berkowitz@gmail.com>
2012-02-29 12:33:02 -0500

    commented functions in header

--- renderfunctions.h
+++ renderfunctions.h
@@ -10,11 +10,27 @@
 #define ROWLENGTH 33
 
 enum currentcolor { BLACK, WHITE };
+
+/* breaks bitboard down into array of individual spaces */
 void decompose(long unsigned int moves,long unsigned int decomposedmoves[]);
+
+/* return a bitboard of adjacent spaces to bitboard */
 long unsigned int getadjacent(long unsigned int board);
+
+/* return a bitboard of empty spaces */
 long unsigned int getempty(long unsigned int black, long unsigned int white);
+
+/* prompts the user for a move or command */
 long unsigned int getmove();
+
+/* return the number of individual moves represented by a moves bitboard */
 int numofmoves(long unsigned int moves);
+
+/* return the number of bits in a long unsigned integer */
 int highestbit();
+
+/* render a horizontal line of specified length */
 void horizontalline(int charlength);
+
+/* render the current board, or a map of moves, if only black is specified */
 void render(long unsigned int black, long unsigned int white);
